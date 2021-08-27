ebe8722902bb93e57eed73934e25498361253313
Fahim <zenothermic@gmail.com>
2021-08-27 15:47:06 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var x = Math.random();
+x = x*6;
+x = Math.floor(x)+1;
+console.log(x);
+
+var y = (Math.random())*6;
+y = Math.floor(y)+1;
+console.log(y);
+
+var randomDiceImg = "images/dice"+(x)+".png";
+var randomDiceImg2 = "images/dice"+(y)+".png";
+
+var img1 = document.querySelectorAll("img")[0]
+img1.setAttribute("src", randomDiceImg);
+
+var img2 = document.querySelectorAll("img")[1]
+img2.setAttribute("src", randomDiceImg2);
+
+if (x>y){
+    document.querySelector("h1").innerHTML = "Player 1 wins";
+}
+else if (y>x){
+    document.querySelector("h1").innerHTML = "Player 2 wins";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw";
+}
